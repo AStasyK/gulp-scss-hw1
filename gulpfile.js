a98e234c4399f3b7a30df44cdd8cae70937041dd
ANAKUC <kuchurina.a.a@gmail.com>
2017-04-04 21:32:10 +0300

    Sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('scss', function () {
   return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
 });
  
